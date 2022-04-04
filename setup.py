bb477f2abccc17e4498501e0c1c76acfa857903c
Sebastian Markgraf <Sebastian-Markgraf@t-online.de>
2022-04-04 18:21:53 +0200

    Move pytube to required dependencies

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="distractor_dmc2gym",
-    version="1.0.1C",
+    version="1.0.2",
     author="yitian_yang",
     author_email="ulisb@student.kit.edu",
     description="A gym like wrapper for dm_control with distractions.",
@@ -14,6 +14,7 @@ setup(
         "numpy",
         "imageio",
         "scikit-video",
+        "pytube",
     ],
-    extras_require={"dev": ["pytest", "pre-commit"], "kinetics400": ["pytube"]},
+    extras_require={"dev": ["pytest", "pre-commit"]},
 )
