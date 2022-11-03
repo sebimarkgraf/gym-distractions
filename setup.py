3f5ca0cb5e3b3168159d87c7cbd60aa13f414354
Sebastian Markgraf <Sebastian-Markgraf@t-online.de>
2022-11-03 22:10:07 +0100

    Fix dependency of n-link to have main branch

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="distractor_dmc2gym",
-    version="1.5.0",
+    version="1.5.1",
     author="Sebastian Markgraf",
     author_email="sebastian-markgraf@t-online.de",
     description="A gym like wrapper for dm_control with distractions.",
@@ -15,7 +15,7 @@ setup(
         "imageio",
         "scikit-video",
         "pytube @ git+https://github.com/kinshuk-h/pytube",
-        "n-link-simulator @ git+ssh://git@github.com/sebimarkgraf/n-link-simulator",
+        "n-link-simulator @ git+ssh://git@github.com/sebimarkgraf/n-link-simulator@main",
     ],
     extras_require={"dev": ["pytest", "pre-commit"]},
 )
