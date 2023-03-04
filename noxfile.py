26b9ec57cc265c91d47435b43535e79ec336b89e
Sebastian Markgraf <Sebastian-Markgraf@t-online.de>
2023-03-04 20:46:39 +0100

    Add test workflow

---
+++ noxfile.py
@@ -0,0 +1,20 @@
+import nox
+
+
+@nox.session
+def lint(session):
+    session.install("pre-commit")
+    session.run("pre-commit", "run", "--all-files", "--show-diff-on-failure")
+
+
+@nox.session(venv_backend="none")
+def test(session):
+    session.run("pdm", "sync", "-d", "-G", "dev", external=True)
+    session.run(
+        "pdm",
+        "run",
+        "pytest",
+        "--cov=gym_distractions",
+        "--cov-report=term-missing",
+        *session.posargs
+    )
